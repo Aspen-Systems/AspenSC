25caa48bdb63c497f7f45bc739d4c6ad03e30c87
rhfrench <ryanfrench@ymail.com>
2015-01-22 14:20:31 -0700

    SQLite

--- app/src/main/java/com/aspen/aspensc/Routes.java
+++ app/src/main/java/com/aspen/aspensc/Routes.java
@@ -1,8 +1,32 @@
 package com.aspen.aspensc;
 
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.content.Context;
+
 /**
- * Created by rfrench on 1/20/2015.
+ * Created by rfrench on 1/21/2015.
  */
-public class Routes
+public class Routes extends SQLiteOpenHelper
 {
+
+    private static final String DBName= "Routes";
+    private static final int DBVer= 1;
+
+    public AspenSQLite(Context context)
+    {
+        super(context, DBName, null, DBVer);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db)
+    {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
+    {
+
+    }
 }
