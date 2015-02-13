53c3c6a5006a25bb288da1b4558014cdf82c5134
rhfrench <ryanfrench@ymail.com>
2015-02-12 17:22:45 -0700

    SQLite

--- app/src/main/java/com/aspen/aspensc/Routes.java
+++ app/src/main/java/com/aspen/aspensc/Routes.java
@@ -12,18 +12,28 @@ public class Routes extends SQLiteOpenHelper
 
     private static final String DBName= "AspenMobile.db";
     private static final int DBVer= 1;
-    private static final String TableName = "OE_CodeRoutes";
+    private static final String mTableName = "OE_CodeRoutes";
 
-    private static final String SQL_CREATE_ENTRIES =
-            "CREATE TABLE " + TableName + " (" +
-                    "RouteCode" + " INTEGER PRIMARY KEY," +
-                    FeedEntry.COLUMN_NAME_ENTRY_ID + TEXT_TYPE + COMMA_SEP +
-                    FeedEntry.COLUMN_NAME_TITLE + TEXT_TYPE + COMMA_SEP +
-    ... // Any other options for the CREATE command
-            " )";
+    private static final String SQL_CREATE_TABLE ="CREATE TABLE `OE_CodeRoutes` (\n" +
+            "\t`RouteCode`\tINTEGER NOT NULL,\n" +
+            "\t`Description1`\tTEXT NOT NULL,\n" +
+            "\t`Description2`\tTEXT,\n" +
+            "\t`Display`\tTEXT NOT NULL,\n" +
+            "\t`TruckCode`\tINTEGER,\n" +
+            "\t`USDA_Destination`\tINTEGER,\n" +
+            "\t`USDA_FOBPrice`\tNUMERIC NOT NULL,\n" +
+            "\t`DeliverOnMonday`\tINTEGER NOT NULL,\n" +
+            "\t`DeliverOnTuesday`\tINTEGER NOT NULL,\n" +
+            "\t`DeliverOnWednesday`\tINTEGER NOT NULL,\n" +
+            "\t`DeliverOnThursday`\tINTEGER NOT NULL,\n" +
+            "\t`DeliverOnFriday`\tINTEGER NOT NULL,\n" +
+            "\t`DeliverOnSaturday`\tINTEGER NOT NULL,\n" +
+            "\t`DeliverOnSunday`\tINTEGER NOT NULL,\n" +
+            "\tPRIMARY KEY(RouteCode)\n" +
+            ");";
 
     private static final String SQL_DELETE_ENTRIES =
-            "DROP TABLE IF EXISTS " + FeedEntry.TABLE_NAME;
+            "DROP TABLE IF EXISTS " + mTableName;
 
 
     // Database creation sql statement
